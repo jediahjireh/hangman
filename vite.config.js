a7c21708b7c5c8099252d4b8c9503bd338240542
Jediah Jireh Naicker <162832021+jediahjireh@users.noreply.github.com>
2024-06-22 18:01:24 +0200

    Update vite.config.js
    Remove specification of Bootstrap as an external dependency

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // explicitly declare Bootstrap as an external module
-  build: {
-    rollupOptions: {
-      external: [
-        'bootstrap/dist/css/bootstrap.min.css',
-      ],
-    },
-  },
-});
+})
