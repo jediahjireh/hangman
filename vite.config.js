451609c36770df818c8c8f1dae45415688992ad3
Jediah Jireh Naicker <162832021+jediahjireh@users.noreply.github.com>
2024-06-22 17:42:39 +0200

    Update vite.config.js
    Format code

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-// explicitly declare Bootstrap as an external module
-build: {
+  // explicitly declare Bootstrap as an external module
+  build: {
     rollupOptions: {
       external: [
         'bootstrap/dist/css/bootstrap.min.css',
